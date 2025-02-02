87734e4254ba936cea03d414e30d6d04b3edf6c0
gnart <nvtrang91@gmail.com>
2025-02-02 19:21:50 +0700

    minimal first commit

--- test/solidity/LiFiDiamond.t.sol
+++ test/solidity/LiFiDiamond.t.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.17;
 
-import { LiFiDiamond } from "lifi/LiFiDiamond.sol";
-import { LibDiamond } from "lifi/Libraries/LibDiamond.sol";
-import { DiamondCutFacet } from "lifi/Facets/DiamondCutFacet.sol";
-import { DiamondLoupeFacet } from "lifi/Facets/DiamondLoupeFacet.sol";
-import { OwnershipFacet } from "lifi/Facets/OwnershipFacet.sol";
-import { IDiamondCut } from "lifi/Interfaces/IDiamondCut.sol";
-import { DSTest } from "ds-test/test.sol";
-import { Vm } from "forge-std/Vm.sol";
+import {LiFiDiamond} from "lifi/LiFiDiamond.sol";
+import {LibDiamond} from "lifi/Libraries/LibDiamond.sol";
+import {DiamondCutFacet} from "lifi/Facets/DiamondCutFacet.sol";
+import {DiamondLoupeFacet} from "lifi/Facets/DiamondLoupeFacet.sol";
+import {OwnershipFacet} from "lifi/Facets/OwnershipFacet.sol";
+import {IDiamondCut} from "lifi/Interfaces/IDiamondCut.sol";
+import {DSTest} from "ds-test/test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {console2} from "forge-std/console2.sol";
 
 contract LiFiDiamondTest is DSTest {
     Vm internal immutable vm = Vm(HEVM_ADDRESS);
+    // LiFiDiamond
     LiFiDiamond public diamond;
     DiamondCutFacet public diamondCutFacet;
     OwnershipFacet public ownershipFacet;
+
     address public diamondOwner;
 
     event DiamondCut(
@@ -32,27 +35,49 @@ contract LiFiDiamondTest is DSTest {
     error ShouldNotReachThisCode();
 
     function setUp() public {
+        // Set the diamond owner to a specific address for testing
         diamondOwner = address(123456);
-        diamondCutFacet = new DiamondCutFacet();
-        ownershipFacet = new OwnershipFacet();
 
-        // prepare function selector for diamondCut (OwnershipFacet)
+        // Deploy the core facets needed for diamond functionality
+        diamondCutFacet = new DiamondCutFacet(); // Handles adding/replacing/removing facets
+        ownershipFacet = new OwnershipFacet(); // Handles ownership management
+
+        // Step 1: Create an array of function selectors to be added to the diamond
+        // In this case, we're only adding the 'owner()' function from OwnershipFacet
         bytes4[] memory functionSelectors = new bytes4[](1);
         functionSelectors[0] = ownershipFacet.owner.selector;
 
-        // prepare parameters for diamondCut (OwnershipFacet)
+        // Log the function selector for debugging purposes
+        // console2.log("function selector");
+        // console2.logBytes4(functionSelectors[0]);
+
+        // Step 2: Create a FacetCut array that specifies how to modify the diamond
+        // This structure defines which functions from which facets should be added/removed
         LibDiamond.FacetCut[] memory cut = new LibDiamond.FacetCut[](1);
+
+        // Configure the facet cut:
+        // - facetAddress: the contract address of the facet (OwnershipFacet)
+        // - action: Add = adding new functions to the diamond
+        // - functionSelectors: array of function selectors to be added
         cut[0] = LibDiamond.FacetCut({
             facetAddress: address(ownershipFacet),
             action: LibDiamond.FacetCutAction.Add,
             functionSelectors: functionSelectors
         });
 
+        // Log facet details for debugging
+        // console2.log("facetAddress", cut[0].facetAddress);
+        // console2.log("action", uint256(cut[0].action));
+
+        // Finally, deploy the main Diamond contract with:
+        // - diamondOwner: the address that will own the diamond
+        // - diamondCutFacet: the address of the facet that handles diamond modifications
         diamond = new LiFiDiamond(diamondOwner, address(diamondCutFacet));
     }
 
     function test_DeploysWithoutErrors() public {
         diamond = new LiFiDiamond(diamondOwner, address(diamondCutFacet));
+        console2.log("diamond", address(diamond));
     }
 
     function test_ForwardsCallsViaDelegateCall() public {
@@ -98,7 +123,7 @@ contract LiFiDiamondTest is DSTest {
     }
 
     function test_CanReceiveETH() public {
-        (bool success, ) = address(diamond).call{ value: 1 ether }("");
+        (bool success, ) = address(diamond).call{value: 1 ether}("");
         if (!success) revert("ExternalCallFailed");
 
         assertEq(address(diamond).balance, 1 ether);
